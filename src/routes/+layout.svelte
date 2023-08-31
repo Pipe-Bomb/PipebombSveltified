d7ea55d8d1d860fa3cde32ef52dd154eb1929693
Mathias <mathiasclari@noximity.com>
2023-08-31 23:32:14 +0200

    feat: Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<h1>Hey</h1>
+<slot />
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h3>Home page</h3>
+
+
+<style lang="scss">
+
+    @import "../variable.scss";
+    h3{
+        color: $primary;
+    }
+</style>
